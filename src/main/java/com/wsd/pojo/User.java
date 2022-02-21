59a16a0c80788018a2e6fc6f4c1100f05284f7a7
wsd <292467402@qq.com>
2022-02-21 12:16:29 +0800

    day31-03-json 第一次提交

---
+++ src/main/java/com/wsd/pojo/User.java
@@ -0,0 +1,18 @@
+package com.wsd.pojo;
+
+import lombok.Data;
+
+/**
+ * @program: wsd165
+ * @description
+ * @author: wansidong
+ * @create: 2022-02-21 12:05
+ */
+@Data
+public class User {
+
+    private Integer id;
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/com/wsd/servlet/LoginServlet.java
@@ -0,0 +1,44 @@
+package com.wsd.servlet;
+
+import com.alibaba.fastjson.JSON;
+import com.wsd.pojo.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * @program: wsd165
+ * @description $description
+ * @author: wansidong
+ * @create: 2022-02-21 12:02
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        BufferedReader reader = request.getReader();
+
+        String json = reader.readLine();
+        System.out.println("json = " + json);
+        //把json字符串转成Java对象
+        User user = JSON.parseObject(json, User.class);
+        System.out.println("user = " + user);
+
+        //把Java对象转成json对象
+        String jsonString = JSON.toJSONString(user);
+        System.out.println("jsonString = " + jsonString);
+
+    }
+
+    @Override
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
